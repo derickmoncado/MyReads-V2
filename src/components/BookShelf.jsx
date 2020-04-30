f5acf6f8cc2ef3b9df498d667bf4b1cb99ceab81
Derick Moncado <derickmoncado@gmail.com>
2020-04-30 15:21:04 -0700

    Added PropTypes from npm.

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -22,7 +22,7 @@ class BookShelf extends Component {
 											}}>
 										</div>
 										<div className="book-shelf-changer">
-											<select onChange={(e) => this.props.changeShelf(book)}>
+											<select value={book.shelf} onChange={(e) => this.props.changeShelf(book => e.target.value)}>
 												<option value="move" disabled>Move to...</option>
 												<option value="currentlyReading">Currently Reading</option>
 												<option value="wantToRead">Want to Read</option>
