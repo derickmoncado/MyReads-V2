4d14a46d6f593fc60e3a80e28d4d5f1b4cfcfcae
Derick Moncado <derickmoncado@gmail.com>
2020-05-07 07:37:15 -0700

    Fixed routing issue from Search page.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Search extends Component {
 	render() {
 		return (
 			<div className="search-books">
 				<div className="search-books-bar">
-					<button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+					<Link to="/" className="close-search">
+						Close
+					</Link>
 					<div className="search-books-input-wrapper">
 						<input type="text" placeholder="Search by title or author"/>
 					</div>
