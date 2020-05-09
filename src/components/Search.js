0ad1da98a8278deefa24b79e63fa38aafba9e7ed
Derick Moncado <derickmoncado@gmail.com>
2020-05-08 17:28:46 -0700

    Work on Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,19 +3,29 @@ import { Link } from 'react-router-dom'
 import * as BooksAPI from '../BooksAPI'
 
 class Search extends Component {
+
 	state = {
-		inputSearchValue: ''
+		query: '',
+		inputSearchValue: '',
 	}
 
-	handleSearch = (inputEvent) => {
-		console.log(inputEvent);
-		BooksAPI.search(inputEvent).then(res => {
-			this.setState(() => ({
-				inputSearchValue: res
-			}));
-			console.log(res);
-		})
+	// handleSearch = (inputEvent) => {
+	// 	console.log("Input Event:", inputEvent);
+	// 	BooksAPI.search(inputEvent).then(res => {
+	// 		this.setState(() => ({
+	// 			inputSearchValue: res
+	// 		}));
+	// 		console.log("Response:", res);
+	// 	})
+	// }
+
+	updateQuery = (query) => {
+		this.setState(() => ({
+			query: query.trim()
+		}))
 	}
+
+
 
 	render() {
 		return (
@@ -23,16 +33,24 @@ class Search extends Component {
 				<div className="search-books-bar">
 					<Link to="/" className="close-search">Close</Link>
 					<div className="search-books-input-wrapper">
+						{/* }<input
+							type="text"
+							placeholder="Search by title or author"
+							value = {this.state.inputEvent}
+							onChange={(e) => this.handleSearch(e.target.value)}
+						/> */}
+
 						<input
 							type="text"
 							placeholder="Search by title or author"
-							value = {this.state.inputSearchValue}
-							onChange={(inputEvent) => this.handleSearch(inputEvent.target.value)}
+							value = {this.state.query}
+							onChange={(event) => this.updateQuery(event.target.value)}
 						/>
 					</div>
 				</div>
 				<div className="search-books-results">
 					<ol className="books-grid">
+						<li>TESTING: {JSON.stringify(this.state)} </li>
 					</ol>
 				</div>
 			</div>
