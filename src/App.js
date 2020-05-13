255cb01ebff33629ea6ef50682ed14783044f668
Derick Moncado <derickmoncado@gmail.com>
2020-05-13 09:30:50 -0700

    Implemented Switch into the Router.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from        './components/Header'
 import Search from        './components/Search'
 import SearchButton from  './components/SearchButton'
 import Shelves from       './components/Shelves'
-import { Route } from 		'react-router-dom'
+import { Route, Switch } from 		'react-router-dom'
 
 class BooksApp extends React.Component {
 	state = {
@@ -35,25 +35,25 @@ class BooksApp extends React.Component {
 	render() {
 		return (
 			<div className="app">
+				<Switch>
+					<Route exact path='/'>
+						<div className="list-books">
+							<Header />
+							<Shelves
+								allTheBooks={this.state.allTheBooks}
+								changeShelf={this.changeShelf}
+							/>
+							<SearchButton />
+						</div>
+					</Route>
 
-				<Route exact path='/' render={() => (
-					<div className="list-books">
-						<Header />
-						<Shelves
-							allTheBooks={this.state.allTheBooks}
-							changeShelf={this.changeShelf}
-						/>
-						<SearchButton />
-					</div>
-				)} />
-
-				<Route exact path='/search' render={() => (
+					<Route path='/search'>
 						<Search
 							shelfBooks={this.state.allTheBooks}
 							onChangeShelf={this.changeShelf}
 						/>
-				)} />
-
+					</Route>
+				</Switch>
 			</div>
 		)
 	}
